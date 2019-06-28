53c00d70d61c5ece9d33f92e51f66d1322780140
Debora Piu <dpiu@uk-c02xl4e8jg5j.uk.deloitte.com>
2019-06-28 14:35:06 +0100

    int k is taken in cosideration and returns empty when under 0

--- lib/longest_consec.rb
+++ lib/longest_consec.rb
@@ -1,6 +1,12 @@
 class LongestConsec
 
-  def longest_consec(args)
-    args.join("")
+  def initialize
+    @building_consec = ""
+    @longest_consec = ""
+  end
+
+  def longest_consec(args, k)
+    @longest_consec = args.join("") if k > 0
+    return @longest_consec
   end
 end
--- spec/longest_consec_spec.rb
+++ spec/longest_consec_spec.rb
@@ -5,12 +5,20 @@ RSpec.describe LongestConsec do
   context "longest_consec" do
     it "returns two words concatenated when arrqay of 2 and k = 2" do
       array = ["wordone", "wordtwo"]
-      expect(subject.longest_consec(array)).to eq "wordonewordtwo"
+      k = 2
+      expect(subject.longest_consec(array, k)).to eq "wordonewordtwo"
     end
 
     it "returns empty when array is empty" do
       array = []
-      expect(subject.longest_consec(array)).to eq ""
+      k = 3
+      expect(subject.longest_consec(array, k)).to eq ""
+    end
+
+    it "returns empty when k < 0" do
+      array = ["words", "anotherword"]
+      k = -2
+      expect(subject.longest_consec(array, k)).to eq ""
     end
   end
 
